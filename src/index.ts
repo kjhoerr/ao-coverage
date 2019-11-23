5964ec16673bd1474d32ffe87a2009b823c36f12
Kevin J Hoerr <kjhoerr@protonmail.com>
2019-11-23 12:29:32 -0500

    Add Prettier to project
    Signed-off-by: Kevin J Hoerr <kjhoerr@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import {badgen} from 'badgen';
-import path from 'path';
-import fs from 'fs';
+import dotenv from "dotenv";
+import express from "express";
+import { badgen } from "badgen";
+import path from "path";
+import fs from "fs";
 
-import formats from './formats';
-import metadata from './metadata';
+import formats from "./formats";
+import metadata from "./metadata";
 
 // Start-up configuration
 dotenv.config();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const TOKEN = process.env.TOKEN || "";
-const HOST_DIR = process.env.HOST_DIR || (() => {
-  throw Error("HOST_DIR must be defined");
-})();
+const HOST_DIR =
+  process.env.HOST_DIR ||
+  (() => {
+    throw Error("HOST_DIR must be defined");
+  })();
 
 const app: express.Application = express();
 
 // serve script for posting coverage report
-app.use('/bash', express.static(path.join(__dirname, '..', 'public', 'bash')));
+app.use("/bash", express.static(path.join(__dirname, "..", "public", "bash")));
 
-// Upload HTML file 
-app.post('/v1/:org/:repo/:branch/:commit.html', (req, res) => {
+// Upload HTML file
+app.post("/v1/:org/:repo/:branch/:commit.html", (req, res) => {
+  const { org, repo, branch, commit } = req.params;
+  console.info(
+    "POST request to /v1/%s/%s/%s/%s.html",
+    org,
+    repo,
+    branch,
+    commit
+  );
 
-  const {org, repo, branch, commit} = req.params;
-  console.info("POST request to /v1/%s/%s/%s/%s.html", org, repo, branch, commit);
-
-  const {token, format} = req.query;
+  const { token, format } = req.query;
   //TODO @Metadata token should come from metadata
   if (token != TOKEN) {
     return res.status(401).send();
@@ -50,7 +57,7 @@ app.post('/v1/:org/:repo/:branch/:commit.html', (req, res) => {
     label: "coverage",
     status: Math.floor(coverage).toString() + "%",
     //TODO @Metadata stage values should come from metadata
-    color: formatter.match_color(coverage, 95, 80),
+    color: formatter.match_color(coverage, 95, 80)
   });
   //TODO store coverage % badge at %HOST_DIR%/%org%/%repo%/%commit%/badge.svg
   //TODO store report file at %HOST_DIR%/%org%/%repo%/%commit%/index.html
@@ -60,8 +67,8 @@ app.post('/v1/:org/:repo/:branch/:commit.html', (req, res) => {
   return res.status(501).send();
 });
 
-app.get('/v1/:org/:repo/:branch.svg', (req, res) => {
-  const {org, repo, branch} = req.params;
+app.get("/v1/:org/:repo/:branch.svg", (req, res) => {
+  const { org, repo, branch } = req.params;
   console.info("GET request to /v1/%s/%s/%s.svg", org, repo, branch);
 
   //TODO @Metadata get the commit @@ via metadata
@@ -69,8 +76,8 @@ app.get('/v1/:org/:repo/:branch.svg', (req, res) => {
   return res.status(501).send();
 });
 
-app.get('/v1/:org/:repo/:branch.html', (req, res) => {
-  const {org, repo, branch} = req.params;
+app.get("/v1/:org/:repo/:branch.html", (req, res) => {
+  const { org, repo, branch } = req.params;
   console.info("GET request to /v1/%s/%s/%s.html", org, repo, branch);
 
   //TODO @Metadata get the commit @@ via metadata
@@ -79,8 +86,8 @@ app.get('/v1/:org/:repo/:branch.html', (req, res) => {
 });
 
 // provide hard link for commit
-app.get('/v1/:org/:repo/:branch/:commit.svg', (req, res) => {
-  const {org, repo, branch, commit} = req.params;
+app.get("/v1/:org/:repo/:branch/:commit.svg", (req, res) => {
+  const { org, repo, branch, commit } = req.params;
   console.info("GET request to /v1/%s/%s/%s/%s.svg", org, repo, branch, commit);
 
   //TODO send the badge file
@@ -88,14 +95,20 @@ app.get('/v1/:org/:repo/:branch/:commit.svg', (req, res) => {
 });
 
 // provide hard link for commit
-app.get('/v1/:org/:repo/:branch/:commit.html', (req, res) => {
-  const {org, repo, branch, commit} = req.params;
-  console.info("GET request to /v1/%s/%s/%s/%s.html", org, repo, branch, commit);
-  
+app.get("/v1/:org/:repo/:branch/:commit.html", (req, res) => {
+  const { org, repo, branch, commit } = req.params;
+  console.info(
+    "GET request to /v1/%s/%s/%s/%s.html",
+    org,
+    repo,
+    branch,
+    commit
+  );
+
   //TODO send the report file
   return res.status(501).send();
 });
 
 app.listen(PORT, () => {
-  console.log('Express started on port ' + PORT);
+  console.log("Express started on port " + PORT);
 });
